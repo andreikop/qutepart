379253710820a6d218f84cd78d02d2c3e6a3ea02
Andrei Kopats <hlamer@tut.by>
2012-07-21 09:49:38 +0300

    Test, which opens all xml files one by one

---
+++ tests/test_xml_definition_parsing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+
+import sys
+sys.path.insert(0, '..')
+from qutepart.Syntax import Syntax
+
+class XmlParsingTestCase(unittest.TestCase):
+    def test_parse_all_definitions(self):
+        """Parse all definitions
+        Test, if we can open all xml files without exceptions.
+        """
+        xmlFilesPath = os.path.join(os.path.dirname(__file__), '..', 'qutepart', 'syntax')
+        for xmlFileName in os.listdir(xmlFilesPath):
+            if xmlFileName.endswith('.xml'):
+                Syntax(xmlFileName)
+
+if __name__ == '__main__':
+    unittest.main()
