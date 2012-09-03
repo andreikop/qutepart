ab74bf6d0a668acffa7c0fd1422f4de1c9cd6f36
Andrei Kopats <hlamer@tut.by>
2012-09-03 09:14:24 +0300

    Test, which opens file by name

---
+++ editor.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+
+import sip
+sip.setapi('QString', 2)
+
+from PyQt4.QtGui import QApplication, QPlainTextEdit, QSyntaxHighlighter, \
+    QTextCharFormat, QTextBlockUserData
+
+from qutepart.SyntaxHighlighter import SyntaxHighlighter
+from qutepart.syntax_manager import SyntaxManager
+
+
+def main():
+    if len(sys.argv) != 2:
+        print 'Usage:\n\t%s FILE' % sys.argv[0]
+
+    filePath = sys.argv[1]
+    
+    try:
+        syntax = SyntaxManager().getSyntaxBySourceFileName(filePath)
+    except KeyError:
+        print 'No syntax for', filePath
+        return
+    
+    print 'Using syntax', syntax.name
+
+    with open(filePath) as file:
+        text = file.read()
+
+    app = QApplication(sys.argv)
+    
+    pte = QPlainTextEdit()
+    pte.setPlainText(text)
+    pte.setWindowTitle(filePath)
+    
+    hl = SyntaxHighlighter(syntax, pte.document())
+    pte.show()
+    return app.exec_()
+    
+
+if __name__ == '__main__':
+    main()
