e3afb28ecdcc5ffc5d8b4b47fe6d3c157155f595
Andrei Kopats <hlamer@tut.by>
2012-12-14 10:21:56 +0300

    Helper scripts

---
+++ makeres.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+#env LD_PRELOAD="/usr/local/lib/libprofiler.so" python -m yep ./editor.py verybigfile.cpp
+
+python ./editor.py verybigfile.cpp
---
+++ showres.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+pprof -gv  --focus=Parser_parseBlock /usr/local/bin/python --add_lib=build/lib.linux-x86_64-2.7/qutepart/cParser.so  python-editor.profile
