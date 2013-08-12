410f378e1d26197c4904a543a981f811f36a9396
Andrei Kopats <hlamer@tut.by>
2013-08-12 19:59:30 +0300

    Return to version 1.0.0

--- qutepart/__init__.py
+++ qutepart/__init__.py
@@ -19,7 +19,7 @@ from qutepart.indenter import getIndenter
 from qutepart.completer import Completer
 from qutepart.lines import Lines
 
-VERSION = (1, 0, 2)
+VERSION = (1, 0, 0)
 
 logger = logging.getLogger('qutepart')
 consoleHandler = logging.StreamHandler()
