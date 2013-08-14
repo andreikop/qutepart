652a1331020540c36c8c079e543a744d017f533f
Andrei Kopats <hlamer@tut.by>
2013-08-14 09:43:07 +0300

    Update version in the docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2013, Andrei Kopats'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
 release = '0.1'
 
