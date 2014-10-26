4ea6f7bda93624b319e33709735ab7423a4b6013
Andrei Kopats <hlamer@tut.by>
2014-10-26 14:26:50 +0300

    completer: fix possible crash. Close #237

--- qutepart/completer.py
+++ qutepart/completer.py
@@ -347,6 +347,7 @@ class Completer(QObject):
         qpart.textChanged.connect(self._onTextChanged)
 
         self.destroyed.connect(self.del_)
+        self._qpart.destroyed.connect(self.del_)
 
     def del_(self):
         """Object deleted. Cancel timer
