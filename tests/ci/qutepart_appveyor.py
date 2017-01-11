8b0858acf29eae14c1e72a2fe913069020a90767
Bryan A. Jones <bjones@ece.msstate.edu>
2017-01-11 15:03:38 -0600

    Fix: Auto-install dependencies.      Test using pre-built PyQt5 wheels (Python 3.5+ only).

--- tests/ci/qutepart_appveyor.py
+++ tests/ci/qutepart_appveyor.py
@@ -31,18 +31,6 @@ def install(should_identify=True):
     if not isdir(DOWNLOADS):
         mkdir(DOWNLOADS)
 
-    # Download and install PyQt5. Only download if we don't have a cached copy
-    # available.
-    install_PyQt5 = os.path.join(DOWNLOADS, 'install-PyQt5.exe')
-    if not isfile(install_PyQt5):
-        wget('http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.5.1/'
-             'PyQt5-5.5.1-gpl-Py3.4-Qt5.5.1-x32.exe',
-              install_PyQt5)
-    # See https://github.com/appveyor/ci/issues/363#issuecomment-148915001.
-    xqt('REG ADD HKCU\\Software\\Python\\PythonCore\\3.4\\InstallPath /f /ve '
-        '/t REG_SZ /d C:\\Python34',
-      install_PyQt5 + ' /S')
-
     # Download and compile PCRE.
     pcre_ver = 'pcre-8.38'
     pcre_zip = pcre_ver + '.zip'
@@ -62,11 +50,14 @@ def install(should_identify=True):
             '-DPCRE_SUPPORT_JIT:BOOL=ON -G "Visual Studio 10 2010"',
           'cmake --build . --config Release')
 
-    # Install, which also builds Python C extensions. Use this instead of
+    # First, build Python C extensions. Use this instead of
     # ``build_ext`` so that Enki will have an already-installed qutepart,
     # rather than needing to regenrate the command below.
-    xqt('python setup.py install --include-dir={}/build '
+    xqt('python setup.py build_ext --include-dir={}/build '
         '--lib-dir={}/build/Release --force'.format(pcre_ver, pcre_ver))
+    # Next, install it along with its dependencies. See comments at
+    # ``install_requires`` on why this is necessary.
+    xqt('python -m pip install -e .')
 #
 # test
 # ====
