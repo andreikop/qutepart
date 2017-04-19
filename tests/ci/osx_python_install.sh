cf3149e3ba3bd33f9fb03139e26eb923d686371f
Bryan A. Jones <bjones@ece.msstate.edu>
2017-04-19 10:50:16 -0500

    Fix: Get CI working on OS X.

--- tests/ci/osx_python_install.sh
+++ tests/ci/osx_python_install.sh
@@ -6,32 +6,9 @@ set -v
 # **********************************************
 # osx_python_install.sh - Install Python on OS X
 # **********************************************
-# Perform the manual steps on OS X to install python3 and
-# activate venv, since Python support is not available, per the list of `unsupported languages on OS X <https://github.com/travis-ci/travis-ci/issues/2320>`_.
-# The following approach is based on a `workaround <https://github.com/travis-ci/travis-ci/issues/2312#issuecomment-195620855>`_.
-# This was modified based on `instructions to install multiple Python versions on OS X <https://gist.github.com/Bouke/11261620>`_.
-# See also the `pyenv docs <https://github.com/yyuu/pyenv/blob/master/README.md>`_.
-#
-# This is fairly noisy, so discard the output.
-brew update > /dev/null
-# Per the `pyenv homebrew recommendations <https://github.com/yyuu/pyenv/wiki#suggested-build-environment>`_.
-brew install openssl readline
-# Since pyenv is already installed, we need to do an upgrade instead.
-# See https://docs.travis-ci.com/user/osx-ci-environment/#A-note-on-upgrading-packages.
-brew outdated pyenv || brew upgrade pyenv
-# virtualenv doesn't work without pyenv knowledge. venv in Python 3.3
-# doesn't provide Pip by default. So, use `pyenv-virtualenv <https://github.com/yyuu/pyenv-virtualenv/blob/master/README.md>`_.
-brew install pyenv-virtualenv
-pyenv install $PYTHON
-# I would expect something like ``pyenv init; pyenv local $PYTHON`` or
-# ``pyenv shell $PYTHON`` would work, but ``pyenv init`` doesn't seem to
-# modify the Bash environment. ??? So, I hand-set the variables instead.
-export PYENV_VERSION=$PYTHON
-export PATH="/Users/travis/.pyenv/shims:${PATH}"
-pyenv-virtualenv venv
+git clone https://github.com/MacPython/terryfy.git
 # Avoid printing the lines from the script below.
 set +v
-source venv/bin/activate
-# A manual check that the correct version of Python is running.
-python --version
+source terryfy/travis_tools.sh
+get_python_environment $pydist $pyver
 
