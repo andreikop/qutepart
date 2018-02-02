c0d34a9bc45225eaebb3f1a63adcad434782c03e
Andrei Kopats <andrei.kopats@gmail.com>
2018-02-02 17:28:33 +0300

    test runner: logging

--- tests/run_all.py
+++ tests/run_all.py
@@ -5,7 +5,14 @@ import sys
 if __name__ == "__main__":
     # Look for all tests. Using test_* instead of test_*.py finds modules (test_syntax and test_indenter).
     suite = unittest.TestLoader().discover('.', pattern="test_*")
+    print("Suite created")
     result = unittest.TextTestRunner(verbosity=2).run(suite)
+    print("Run done")
 
     # Indicate success or failure via the exit code: success = 0, failure = 1.
-    sys.exit(not result.wasSuccessful())
+    if result.wasSuccessful():
+        print("OK")
+        sys.exit(0)
+    else:
+        print("Failed")
+        sys.exit(not result.wasSuccessful())
